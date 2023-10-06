ddee66c0b2ee913536eb66e251afc6120fbd4973
Othman Bella <147095415+Othman-Id-bella@users.noreply.github.com>
2023-10-06 18:57:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ N1=float(input("Give the value of the note N1:"))
 N2=float(input("Give the value of the note N2:"))
 N3=float(input("Give the value of the note N3:"))
            
-
 while (N1<0 or N1>20 ):
    N1=float(input("Give the value of the note N1:"))
 while (N2<0 or N2>20):
     N2=float(input("Give the value of the note N2:"))
 while (N3<0 or N3>20):
     N3=float(input("Give the value of the note N3:"))
-A=(N1+N2+N3)/3  
+A=(N1+N2+N3)/3  #A:averge
 print(A)
 if A>=16:
-    B="Alright"
+    B="Alright"           #B:mention
 elif A<16 and A>14:
     B="Good"
 elif A<14 and A>12:
@@ -21,4 +20,4 @@ elif A<12 and A>10:
     B="fair"
 else:
     B="sufficient"
-print("the studint mintion is:",B)
+print("the student mention is:",B)
